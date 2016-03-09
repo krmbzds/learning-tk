8e357c69536cfd4b7c151b0fe6b5f03ed6797859
Kerem Bozdas <krmbzds.github@gmail.com>
2016-03-09 16:16:01 +0200

    Add second example

---
+++ hello_again.py
@@ -0,0 +1,24 @@
+from tkinter import *
+
+class Example(Frame):
+
+    def __init__(self, parent):
+        Frame.__init__(self, parent, background="white")
+        self.parent = parent
+        self.initUI()
+
+    def initUI(self):
+        self.hi_there = Button(self, text="Hello", command=self.say_hi)
+        self.hi_there.pack()
+        self.pack()
+
+    def say_hi(self):
+        print("Hi there, everyone!")
+
+def main():
+    root = Tk()
+    root.geometry("250x150+300+300")
+    app = Example(root)
+    root.mainloop()
+
+main()
